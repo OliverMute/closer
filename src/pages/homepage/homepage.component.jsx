626790b904b2f5fb416a76a372b76cbf32b95e4a
oliver <olivieralexis@yahoo.com>
2020-06-24 00:30:29 +0200

    responsive

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -5,10 +5,9 @@ import Directory from "../../components/directory/directory.component";
 const Homepage = () => {
   return (
     <Fragment>
-      <div id="homepage-video">
-        <video src="video/veste 3.mp4" id={"veste"} autoPlay muted loop />
-        <video src="video/adidas3-2.mp4" id={"adidas3"} autoPlay muted loop />
-      </div>
+      <div id="homepage-video"></div>
+      <video src="video/adidas3-2.mp4" id={"adidas3"} autoPlay muted loop />
+      <video src="video/veste 3.mp4" id={"veste"} autoPlay muted loop />
       <video src="video/closer 3.mp4" id={"closer3"} autoPlay muted loop />
       <div className="homepage">
         <img id="banner" src="images/banner.jpg" alt="" />
