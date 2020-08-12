1923af4f86cab71563766f0d74facddc285b4d40
oliver <olivieralexis@yahoo.com>
2020-08-12 04:04:50 +0200

    added stripe checkout service in checkout page

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -8,6 +8,8 @@ import { selectCartTotal } from "../../redux/cart/cart.selectors";
 
 import "./checkout.styles.scss";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
+
 import Hat from "../../components/video/hat.component";
 
 const CheckoutPage = ({ cartItems, total }) => {
@@ -37,6 +39,12 @@ const CheckoutPage = ({ cartItems, total }) => {
       <div className={"total"}>
         <span>TOTAL: ${total}</span>
       </div>
+      <div className={"test-warning"}>
+        *Please use the following test credit card for payments*
+        <br />
+        4242 4242 4242 4242 - Exp: 09/20 - CVV: 123
+      </div>
+      <StripeCheckoutButton price={total} />
     </div>
   );
 };
